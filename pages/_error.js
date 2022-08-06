bc5ed097de230deec416980baa3fb98e8fed85bf
Adrienn Lenart <adriennlenart@Blankas-MacBook-Pro.local>
2022-08-06 14:05:08 +0200

    correcting _error file

--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,10 @@
 export async function getStaticProps({res, err}) {
     const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-    console.log('STATUSCODE',statusCode)
-    return {statusCode}
+    return {props: {statusCode: statusCode}}
 }
 
 function Error({statusCode}) {
+    
     return(
         <p>
             {statusCode 
