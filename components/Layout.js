455a2a1a0d896d9f82c1803e424056adf1c2543e
Adrienn Lenart <adriennlenart@Blankas-MacBook-Pro.local>
2022-08-10 17:17:10 +0200

    add authentication

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,20 @@
 import Navbar from "./Navbar";
+import {useSession, signIn} from 'next-auth/react'
+import styles from '../styles/Home.module.css'
 
 const Layout = ({children}) => {
+    const {data: session} = useSession()
+
+    if(!session){
+        return(
+            <div>
+                <Navbar />
+                <p className = {styles.message}>You are currently not signed in.</p>
+                <button className = {styles.googleButton} onClick={() => signIn()}>Sign in with Google</button>
+            </div>
+        )
+    }
+
     return ( 
         <div>
             <Navbar />
