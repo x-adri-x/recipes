c04a9a0b2d325260ffefbd30782113aad2211d58
Adrienn Lenart <adriennlenart@yahoo.com>
2025-01-18 16:02:57 +0100

    fix: correct file name

---
+++ components/MobileNavBar.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import { MdLogout } from 'react-icons/md'
+
+const MobileNavBar = ({ setIsMobileMenuOpen }) => {
+  const handleMenuItemClick = () => {
+    setIsMobileMenuOpen(false)
+  }
+
+  return (
+    <nav className='*:p-2.5 justify-start [&_a]:font-normal [&_a]:border-b-[1px] [&_a]:border-b-slate-100 text-sm text-black [&_a]:uppercase w-full [&_a]:py-6 h-fit tracking-[1.5px] flex flex-col min-h-screen'>
+      <button onClick={() => signOut()}>
+        <MdLogout size='1.5rem' />
+      </button>
+      <Link href='/main' onClick={handleMenuItemClick}>
+        Main
+      </Link>
+      <Link href='/soup' onClick={handleMenuItemClick}>
+        Soups
+      </Link>
+      <Link href='/dessert' onClick={handleMenuItemClick}>
+        Desserts
+      </Link>
+      <Link href='/salad' onClick={handleMenuItemClick}>
+        Salads
+      </Link>
+    </nav>
+  )
+}
+
+export default MobileNavBar
