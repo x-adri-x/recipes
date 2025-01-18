46c4f357f5f26bd51be7a7954c2af361cda37dad
Adrienn Lenart <adriennlenart@yahoo.com>
2025-01-18 20:50:35 +0100

    fix: add origin

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://recipes-one-omega.vercel.app/' },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT',
