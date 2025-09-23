98b96f1b37ff040abba4e25991e78e47db47a68d
Adrienn Lenart <adriennlenart@yahoo.com>
2025-09-23 15:15:57 +0200

    add useQuery for main course index

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,17 @@
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import {SessionProvider} from 'next-auth/react'
+import { SessionProvider } from 'next-auth/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
-function MyApp({ Component, pageProps: {session, ...pageProps} }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session = {session}>
-      <QueryClientProvider client = {queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout> 
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </QueryClientProvider>
     </SessionProvider>
   )
--- pages/main/index.js
+++ pages/main/index.js
@@ -1,32 +1,55 @@
 import PageLayout from '@/components/PageLayout'
 import Heading from '@/components/Heading'
+import { useQuery } from '@tanstack/react-query'
 
-export async function getStaticProps() {
-  const recipes = await fetch(
-    `${process.env.CONTENTFUL_HOST}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries` +
-      `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=main`
-  )
-    .then((response) => {
-      if (response.ok) {
-        return response.json()
-      } else {
-        throw new Error('Failed to fetch data.')
-      }
-    })
-    .catch((error) => console.log(error))
+// export async function getStaticProps() {
+//   const recipes = await fetch(
+//     `${process.env.CONTENTFUL_HOST}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries` +
+//       `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=main`
+//   )
+//     .then((response) => {
+//       if (response.ok) {
+//         return response.json()
+//       } else {
+//         throw new Error('Failed to fetch data.')
+//       }
+//     })
+//     .catch((error) => console.log(error))
 
-  return {
-    props: {
-      recipes: recipes,
-    },
-  }
-}
+//   return {
+//     props: {
+//       recipes: recipes,
+//     },
+//   }
+// }
 
 const MainCourse = ({ recipes }) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['recipes', 'main'],
+    queryFn: fetchData,
+    initialData: recipes,
+  })
+  const fetchData = async () => {
+    const result = await fetch(
+      `${process.env.CONTENTFUL_HOST}/spaces/${process.env.CONTENTFUL_SPACE_ID}/environments/master/entries` +
+        `?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}&content_type=main`
+    ).then((response) => response.json())
+
+    return result
+  }
+
+  if (isLoading) return <div>Loading...</div>
+  if (error)
+    return (
+      <div>
+        <p>Error: {error.message}</p>
+      </div>
+    )
+
   return (
     <>
       <Heading title='Main Course Recipes' />
-      <PageLayout props={recipes} />
+      <PageLayout props={data?.recipes} />
     </>
   )
 }
